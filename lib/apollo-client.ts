891feee9d1970f45e3d1233b0351521c7cfe1a83
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:37:49 -0500

    feat: add Cloudflare Pages compatibility

--- lib/apollo-client.ts
+++ lib/apollo-client.ts
@@ -1,5 +1,6 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
+import { isDemoMode, handleMockQuery } from './demo-mode'
 
 function getServerBaseUrl(): string {
   const explicit = process.env.NEXT_PUBLIC_SITE_URL
@@ -15,6 +16,25 @@ function getGraphqlUri(): string {
   return `${getServerBaseUrl()}/api/graphql`
 }
 
+async function fetchGraphql(
+  uri: RequestInfo | URL,
+  options?: RequestInit,
+  withTags = false
+): Promise<Response> {
+  if (typeof window === 'undefined' && isDemoMode()) {
+    const body = typeof options?.body === 'string' ? options.body : '{}'
+    return new Response(JSON.stringify(handleMockQuery(body)), {
+      headers: { 'Content-Type': 'application/json' },
+    })
+  }
+
+  if (withTags) {
+    return fetch(uri, { ...options, next: { tags: ['drupal'] } } as RequestInit)
+  }
+
+  return fetch(uri, options)
+}
+
 let browserClient: ApolloClient<any> | null = null
 
 export function getServerApolloClient(requestHeaders: Headers): ApolloClient<any> {
@@ -26,7 +46,7 @@ export function getServerApolloClient(requestHeaders: Headers): ApolloClient<any
   const httpLink = createHttpLink({
     uri: `${origin}/api/graphql`,
     fetch: (uri: RequestInfo | URL, options?: RequestInit) =>
-      fetch(uri, { ...options, next: { tags: ['drupal'] } } as RequestInit),
+      fetchGraphql(uri, options, true),
   })
 
   const authLink = setContext((_, { headers }) => ({
