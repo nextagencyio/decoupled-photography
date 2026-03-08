4be8cd92c33fb3d434df1ec1e73779c30bbe917f
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:42:46 -0500

    fix: mark header-dependent routes as dynamic

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import GalleryCard from '../components/GalleryCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Gallery | Lumen Studio',
